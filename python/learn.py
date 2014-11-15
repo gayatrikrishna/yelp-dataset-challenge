ca572c5c0da4752f46d550640125a8d84979a2be
Jason Feriante <jason.feriante@gmail.com>
2014-11-14 23:24:37 -0600

    created smaller data subsets to get going, started working on clustering

--- python/learn.py
+++ python/learn.py
@@ -1,9 +1,62 @@
-import sys, numpy, scipy, matplotlib
+import sys, numpy, scipy, matplotlib, helpers
+from sklearn.cluster import KMeans
+# http://ptg.ucsd.edu/~staal/mit/arff/
+# /usr/local/lib/python2.7/site-packages/arff
+# from arff import arffread
+
+from lib import arff
+
+load_data = helpers.load_data
+get_attributes = helpers.get_attributes
+get_class_counts = helpers.get_class_counts
+get_arguments = helpers.get_arguments
+
+def run_kmeans():
+	KMeans(
+		8, # n_clusters
+		'k-means++', #init
+		10, # n_init
+		10, # max_iter
+		0.0001, #tol
+		True, #precompute_distances
+		# 0, verbose
+		None, #random_state
+		True, #copy_x
+		1, # n_jobs
+	)
+	
+
+def arff_load(filepath):
+	handle = open(filepath)
+	return arffread(handle)
+
+
+def print_sklearn_version():
+	print 'Version of Scikit: ' + sklearn.__version__  
+	path = sklearn.__path__
+	print 'Path to Scikit: ' + str(path[0])
+	exit(0)
 
 def main(args):
-  print "hello"  
+	# print_sklearn_version()
+	# run_kmeans()
+	path = [
+		'../data/arff/subsets/yelp_academic_dataset_user.arff',
+		'../data/arff/subsets/yelp_academic_dataset_business.arff',
+		'../data/arff/subsets/yelp_academic_dataset_checkin.arff',
+		'../data/arff/subsets/yelp_academic_dataset_review.arff',
+		'../data/arff/subsets/yelp_academic_dataset_tip.arff',
+	]
+
+	arff_file = load_data(path[0])
+	attributes = get_attributes(arff_file['attributes'])
+	data = arff_file['data']
+
+	# print attributes
+	print data[0]
+	exit(0)
 
 if __name__ == "__main__":
-  main(sys.argv)
+	main(sys.argv)
 
 
