83be37f91872156d93847c8a5ad0f0d0567fbbb2
Jason Feriante <jason.feriante@gmail.com>
2014-11-21 20:31:54 -0600

    spaces to tabs

--- php/generate_clean_csv.php
+++ php/generate_clean_csv.php
@@ -9,27 +9,27 @@ require('debug.php');
 
 function sort_array_by_keys(array $unsorted, array $sortKeys)
 {
-  if(count($unsorted) != count($sortKeys))
-  {
-    echo Debug::vars($unsorted); exit;
-  }
-  $sorted = [];
-  foreach ($sortKeys as $key => $value)
-  {
-    $sorted[$key] = str_replace(",", ";", $unsorted[$key]);
-  }
+	if(count($unsorted) != count($sortKeys))
+	{
+		echo Debug::vars($unsorted); exit;
+	}
+	$sorted = [];
+	foreach ($sortKeys as $key => $value)
+	{
+		$sorted[$key] = str_replace(",", ";", $unsorted[$key]);
+	}
 
-  return $sorted;
+	return $sorted;
 }
 
 function get_infile($filename)
 {
-  return 'data/csv/'  . $filename;
+	return 'data/csv/'  . $filename;
 }
 
 function get_outfile($filename)
 {
-  return 'data/cleaned-csv/' . $filename;
+	return 'data/cleaned-csv/' . $filename;
 }
 
 // Changes to users: removed type & name
@@ -37,292 +37,292 @@ function get_outfile($filename)
 //  -friends just becomes a count of how many friends this user has (a graph representation would be expensive)
 function generate_user_csv($filename)
 {
-  $infile = get_infile($filename);
-  $outfile = get_outfile($filename);
-  // sanitize: name, city, type, 
-  $handle = fopen($infile, "r");  
-  $handle_out = fopen($outfile, "w"); 
-  
-  // First line
-  $attributes = fgetcsv($handle, 0, ",",'"');
-  // echo Debug::vars($attributes); exit;
-  asort($attributes);
-  // Remove name & type
-  unset($attributes[8]);
-  unset($attributes[15]);
-  $rs = fputcsv($handle_out, $attributes);
+	$infile = get_infile($filename);
+	$outfile = get_outfile($filename);
+	// sanitize: name, city, type, 
+	$handle = fopen($infile, "r");  
+	$handle_out = fopen($outfile, "w"); 
+	
+	// First line
+	$attributes = fgetcsv($handle, 0, ",",'"');
+	// echo Debug::vars($attributes); exit;
+	asort($attributes);
+	// Remove name & type
+	unset($attributes[8]);
+	unset($attributes[15]);
+	$rs = fputcsv($handle_out, $attributes);
 
-  while(($line = fgetcsv($handle, 0, ",",'"')) !== false) 
-  {
-    // Remove name & type
-    unset($line[8]);
-    unset($line[15]);
+	while(($line = fgetcsv($handle, 0, ",",'"')) !== false) 
+	{
+		// Remove name & type
+		unset($line[8]);
+		unset($line[15]);
 
-    $line = sort_array_by_keys($line, $attributes);
+		$line = sort_array_by_keys($line, $attributes);
 
-    if(strlen($line[14]) > 4)
-    {
-      $line[14] = count(explode(",", $line[14]));
-    }
-    else
-    {
-      $line[14] = 0;
-    }
+		if(strlen($line[14]) > 4)
+		{
+			$line[14] = count(explode(",", $line[14]));
+		}
+		else
+		{
+			$line[14] = 0;
+		}
 
-    if(strlen($line[3]) > 4)
-    {
-      $line[3] = count(explode(",", $line[3]));
-    }
-    else
-    {
-      $line[3] = 0;
-    }
+		if(strlen($line[3]) > 4)
+		{
+			$line[3] = count(explode(",", $line[3]));
+		}
+		else
+		{
+			$line[3] = 0;
+		}
 
-    $date = DateTime::createFromFormat('Y-m-d H:i:s', $line[0].'-01 00:00:00');
-    //yyyy-MM-dd HH:mm:ss
-    //"2001-04-03 12:12:12"
-    $line[0] = $date->format('Y-m-d H:i:s');
-    $line[16] = '"'.$line[16].'"';
-    $rs = fputcsv($handle_out, $line);
-  }
-  // if (!feof($handle)) {
-  //  echo "Error: unexpected fgets() fail\n";
-  // }
-  fclose($handle);
-  fclose($handle_out);
+		$date = DateTime::createFromFormat('Y-m-d H:i:s', $line[0].'-01 00:00:00');
+		//yyyy-MM-dd HH:mm:ss
+		//"2001-04-03 12:12:12"
+		$line[0] = $date->format('Y-m-d H:i:s');
+		$line[16] = '"'.$line[16].'"';
+		$rs = fputcsv($handle_out, $line);
+	}
+	// if (!feof($handle)) {
+	//  echo "Error: unexpected fgets() fail\n";
+	// }
+	fclose($handle);
+	fclose($handle_out);
 
-  $contents = file_get_contents($outfile);
-  $contents = str_replace('"""', '"', $contents);
-  file_put_contents($outfile, $contents);
+	$contents = file_get_contents($outfile);
+	$contents = str_replace('"""', '"', $contents);
+	file_put_contents($outfile, $contents);
 }
 
 function generate_business_csv($filename)
 {
-  $infile = get_infile($filename);
-  $outfile = get_outfile($filename);
+	$infile = get_infile($filename);
+	$outfile = get_outfile($filename);
 
-  // sanitize: name, city, type, 
-  $handle = fopen($infile, "r");  
-  $handle_out = fopen($outfile, "w"); 
-  
-  // First line
-  $attributes = fgetcsv($handle, 0, ",",'"');
-  asort($attributes);
-  $rs = fputcsv($handle_out, $attributes);
+	// sanitize: name, city, type, 
+	$handle = fopen($infile, "r");  
+	$handle_out = fopen($outfile, "w"); 
+	
+	// First line
+	$attributes = fgetcsv($handle, 0, ",",'"');
+	asort($attributes);
+	$rs = fputcsv($handle_out, $attributes);
 
-  while(($line = fgetcsv($handle, 0, ",",'"')) !== false) 
-  {
-    $line = sort_array_by_keys($line, $attributes);
-    $rs = fputcsv($handle_out, $line);
-  }
+	while(($line = fgetcsv($handle, 0, ",",'"')) !== false) 
+	{
+		$line = sort_array_by_keys($line, $attributes);
+		$rs = fputcsv($handle_out, $line);
+	}
 
-  fclose($handle);
-  fclose($handle_out);
+	fclose($handle);
+	fclose($handle_out);
 
-  $contents = file_get_contents($outfile);
-  $contents = str_replace('"""', '"', $contents);
-  file_put_contents($outfile, $contents);
+	$contents = file_get_contents($outfile);
+	$contents = str_replace('"""', '"', $contents);
+	file_put_contents($outfile, $contents);
 }
 
 // CSV to clean CSV
 function generate_review($filename)
 {
-  $infile = get_infile($filename);
-  $outfile = get_outfile($filename);
+	$infile = get_infile($filename);
+	$outfile = get_outfile($filename);
 
-  $handle = fopen($infile, "r");  
-  $handle_out = fopen($outfile, "w"); 
-  
-  // First line
-  $attributes = fgetcsv($handle, 0, ",",'"');
-  asort($attributes);
-  $rs = fputcsv($handle_out, $attributes);
+	$handle = fopen($infile, "r");  
+	$handle_out = fopen($outfile, "w"); 
+	
+	// First line
+	$attributes = fgetcsv($handle, 0, ",",'"');
+	asort($attributes);
+	$rs = fputcsv($handle_out, $attributes);
 
-  while(($line = fgetcsv($handle, 0, ",",'"')) !== false) 
-  {
-    $line = sort_array_by_keys($line, $attributes);
-    $rs = fputcsv($handle_out, $line);
-  }
+	while(($line = fgetcsv($handle, 0, ",",'"')) !== false) 
+	{
+		$line = sort_array_by_keys($line, $attributes);
+		$rs = fputcsv($handle_out, $line);
+	}
 
-  fclose($handle);
-  fclose($handle_out);
+	fclose($handle);
+	fclose($handle_out);
 
-  $contents = file_get_contents($outfile);
-  $contents = str_replace('"""', '"', $contents);
-  file_put_contents($outfile, $contents);
+	$contents = file_get_contents($outfile);
+	$contents = str_replace('"""', '"', $contents);
+	file_put_contents($outfile, $contents);
 }
 
 // CSV to clean CSV
 function generate_tip($filename)
 {
-  $infile = get_infile($filename);
-  $outfile = get_outfile($filename);
+	$infile = get_infile($filename);
+	$outfile = get_outfile($filename);
 
-  $handle = fopen($infile, "r");  
-  $handle_out = fopen($outfile, "w"); 
-  
-  // First line
-  $attributes = fgetcsv($handle, 0, ",",'"');
-  asort($attributes);
-  $rs = fputcsv($handle_out, $attributes);
+	$handle = fopen($infile, "r");  
+	$handle_out = fopen($outfile, "w"); 
+	
+	// First line
+	$attributes = fgetcsv($handle, 0, ",",'"');
+	asort($attributes);
+	$rs = fputcsv($handle_out, $attributes);
 
-  while(($line = fgetcsv($handle, 0, ",",'"')) !== false) 
-  {
-    $line = sort_array_by_keys($line, $attributes);
-    $rs = fputcsv($handle_out, $line);
-  }
+	while(($line = fgetcsv($handle, 0, ",",'"')) !== false) 
+	{
+		$line = sort_array_by_keys($line, $attributes);
+		$rs = fputcsv($handle_out, $line);
+	}
 
-  fclose($handle);
-  fclose($handle_out);
+	fclose($handle);
+	fclose($handle_out);
 
-  $contents = file_get_contents($outfile);
+	$contents = file_get_contents($outfile);
 }
 
 // CSV to clean CSV
 function generate_checkin($filename)
 {
-  $infile = get_infile($filename);
-  $outfile = get_outfile($filename);
+	$infile = get_infile($filename);
+	$outfile = get_outfile($filename);
 
-  $handle = fopen($infile, "r");  
-  $handle_out = fopen($outfile, "w"); 
-  
-  // First line
-  $attributes = fgetcsv($handle, 0, ",",'"');
-  asort($attributes);
-  unset($attributes[103]);
-  $rs = fputcsv($handle_out, $attributes);
+	$handle = fopen($infile, "r");  
+	$handle_out = fopen($outfile, "w"); 
+	
+	// First line
+	$attributes = fgetcsv($handle, 0, ",",'"');
+	asort($attributes);
+	unset($attributes[103]);
+	$rs = fputcsv($handle_out, $attributes);
 
-  while(($line = fgetcsv($handle, 0, ",",'"')) !== false) 
-  {
-    unset($line[103]);
-    $line = sort_array_by_keys($line, $attributes);
-    $rs = fputcsv($handle_out, $line);
-  }
+	while(($line = fgetcsv($handle, 0, ",",'"')) !== false) 
+	{
+		unset($line[103]);
+		$line = sort_array_by_keys($line, $attributes);
+		$rs = fputcsv($handle_out, $line);
+	}
 
-  fclose($handle);
-  fclose($handle_out);
+	fclose($handle);
+	fclose($handle_out);
 
-  $contents = file_get_contents($outfile);
+	$contents = file_get_contents($outfile);
 }
 
 // JSON to csv
 function generate_review_csv($filename)
 {
-  $infile = get_infile($filename);
-  $outfile = get_outfile($filename);
+	$infile = get_infile($filename);
+	$outfile = get_outfile($filename);
 
-  $infile = str_replace(".csv", ".json", $infile);
-  $handle = fopen($infile, "r");  
-  $handle_out = fopen($outfile, "w"); 
-  
-  // First line
-  $json = fgets($handle);
-  $data = json_decode($json);
-  
-  $attributes = ['votes.funny', 'votes.useful', 'votes.cool', 'user_id', 'review_id', 'stars', 'date', 'business_id'];
-  $rs = fputcsv($handle_out, $attributes);
+	$infile = str_replace(".csv", ".json", $infile);
+	$handle = fopen($infile, "r");  
+	$handle_out = fopen($outfile, "w"); 
+	
+	// First line
+	$json = fgets($handle);
+	$data = json_decode($json);
+	
+	$attributes = ['votes.funny', 'votes.useful', 'votes.cool', 'user_id', 'review_id', 'stars', 'date', 'business_id'];
+	$rs = fputcsv($handle_out, $attributes);
 
-  $date = DateTime::createFromFormat('Y-m-d H:i:s', $data->date.' 00:00:00');
-  $the_date = $date->format('Y-m-d H:i:s');
+	$date = DateTime::createFromFormat('Y-m-d H:i:s', $data->date.' 00:00:00');
+	$the_date = $date->format('Y-m-d H:i:s');
 
-  // write first line:
-  $line = [
-    $data->votes->funny,
-    $data->votes->useful,
-    $data->votes->cool,
-    $data->user_id,
-    $data->review_id,
-    $data->stars,
-    $the_date,
-    $data->business_id
-  ];
+	// write first line:
+	$line = [
+		$data->votes->funny,
+		$data->votes->useful,
+		$data->votes->cool,
+		$data->user_id,
+		$data->review_id,
+		$data->stars,
+		$the_date,
+		$data->business_id
+	];
 
-  $rs = fputcsv($handle_out, $line);
+	$rs = fputcsv($handle_out, $line);
 
-  while(($json = fgets($handle)) !== false) 
-  {
-    $data = json_decode($json);
-    $date = DateTime::createFromFormat('Y-m-d H:i:s', $data->date.' 00:00:00');
-    $the_date = $date->format('Y-m-d H:i:s');
+	while(($json = fgets($handle)) !== false) 
+	{
+		$data = json_decode($json);
+		$date = DateTime::createFromFormat('Y-m-d H:i:s', $data->date.' 00:00:00');
+		$the_date = $date->format('Y-m-d H:i:s');
 
-    // write first line:
-    $line = [
-      $data->votes->funny,
-      $data->votes->useful,
-      $data->votes->cool,
-      $data->user_id,
-      $data->review_id,
-      $data->stars,
-      $the_date,
-      $data->business_id
-    ];
+		// write first line:
+		$line = [
+			$data->votes->funny,
+			$data->votes->useful,
+			$data->votes->cool,
+			$data->user_id,
+			$data->review_id,
+			$data->stars,
+			$the_date,
+			$data->business_id
+		];
 
-    $rs = fputcsv($handle_out, $line);
-  }
+		$rs = fputcsv($handle_out, $line);
+	}
 
-  fclose($handle);
-  fclose($handle_out);
+	fclose($handle);
+	fclose($handle_out);
 
-  $contents = file_get_contents($outfile);
+	$contents = file_get_contents($outfile);
 }
 
 function generate_tip_csv($filename)
 {
-  $infile = get_infile($filename);
-  $outfile = get_outfile($filename);
+	$infile = get_infile($filename);
+	$outfile = get_outfile($filename);
 
-  $infile = str_replace(".csv", ".json", $infile);
-  $handle = fopen($infile, "r");  
-  $handle_out = fopen($outfile, "w"); 
-  
-  // First line
-  $json = fgets($handle);
-  $data = json_decode($json);
+	$infile = str_replace(".csv", ".json", $infile);
+	$handle = fopen($infile, "r");  
+	$handle_out = fopen($outfile, "w"); 
+	
+	// First line
+	$json = fgets($handle);
+	$data = json_decode($json);
 
-  // public user_id => string(22) "Vefj29mjork1DLhALLNAsg"
-  // public text => string(54) "Great food, huge portions and a gift shop and showers."
-  // public business_id => string(22) "JwUE5GmEO-sH1FuwJgKBlQ"
-  // public likes => integer 0
-  // public date => string(10) "2012-05-16"
-  // public type => string(3) "tip"
-    
-  $attributes = ['user_id', 'business_id', 'likes', 'date'];
-  $rs = fputcsv($handle_out, $attributes);
+	// public user_id => string(22) "Vefj29mjork1DLhALLNAsg"
+	// public text => string(54) "Great food, huge portions and a gift shop and showers."
+	// public business_id => string(22) "JwUE5GmEO-sH1FuwJgKBlQ"
+	// public likes => integer 0
+	// public date => string(10) "2012-05-16"
+	// public type => string(3) "tip"
+		
+	$attributes = ['user_id', 'business_id', 'likes', 'date'];
+	$rs = fputcsv($handle_out, $attributes);
 
-  $date = DateTime::createFromFormat('Y-m-d H:i:s', $data->date.' 00:00:00');
-  $the_date = $date->format('Y-m-d H:i:s');
+	$date = DateTime::createFromFormat('Y-m-d H:i:s', $data->date.' 00:00:00');
+	$the_date = $date->format('Y-m-d H:i:s');
 
-  // write first line:
-  $line = [
-    $data->user_id,
-    $data->business_id,
-    $data->likes,
-    $the_date
-  ];
+	// write first line:
+	$line = [
+		$data->user_id,
+		$data->business_id,
+		$data->likes,
+		$the_date
+	];
 
-  $rs = fputcsv($handle_out, $line);
+	$rs = fputcsv($handle_out, $line);
 
-  while(($json = fgets($handle)) !== false) 
-  {
-    $data = json_decode($json);
+	while(($json = fgets($handle)) !== false) 
+	{
+		$data = json_decode($json);
 
-    $date = DateTime::createFromFormat('Y-m-d H:i:s', $data->date.' 00:00:00');
-    $the_date = $date->format('Y-m-d H:i:s');
-    $line = [
-      $data->user_id,
-      $data->business_id,
-      $data->likes,
-      $the_date
-    ];
+		$date = DateTime::createFromFormat('Y-m-d H:i:s', $data->date.' 00:00:00');
+		$the_date = $date->format('Y-m-d H:i:s');
+		$line = [
+			$data->user_id,
+			$data->business_id,
+			$data->likes,
+			$the_date
+		];
 
-    $rs = fputcsv($handle_out, $line);
-  }
+		$rs = fputcsv($handle_out, $line);
+	}
 
-  fclose($handle);
-  fclose($handle_out);
+	fclose($handle);
+	fclose($handle_out);
 
-  $contents = file_get_contents($outfile);
+	$contents = file_get_contents($outfile);
 }
 
 echo "<pre>starting task\n</pre>";
--- php/remove_unknowns.php
+++ php/remove_unknowns.php
@@ -13,156 +13,156 @@ require('debug.php');
 
 function get_infile($filename)
 {
-  return 'data/csv/'  . $filename;
+	return 'data/csv/'  . $filename;
 }
 
 function get_outfile($filename)
 {
-  return 'data/cleaned-csv/' . $filename;
+	return 'data/cleaned-csv/' . $filename;
 }
 
 // Remove all "unknowns" from user dataset (replace w/ 0)
 // since in truth, all these things actually are known.
 function generate_user_csv($filename)
 {
-  $infile = get_infile($filename);
-  $outfile = get_outfile($filename);
+	$infile = get_infile($filename);
+	$outfile = get_outfile($filename);
 
-  // sanitize: name, city, type, 
-  $handle = fopen($infile, "r");  
-  $handle_out = fopen($outfile, "w"); 
-  
-  $attributes = fgetcsv($handle, 0, ",",'"');
-  $rs = fputcsv($handle_out, $attributes);
+	// sanitize: name, city, type, 
+	$handle = fopen($infile, "r");  
+	$handle_out = fopen($outfile, "w"); 
+	
+	$attributes = fgetcsv($handle, 0, ",",'"');
+	$rs = fputcsv($handle_out, $attributes);
 
-  while(($line = fgetcsv($handle, 0, ",",'"')) !== false) 
-  {
-    foreach ($line as $key => $val) 
-    {
-      // Don't allow any empty values
-      if($line[$key] == "")
-      {
-        $line[$key] = "0";
-      }
-    }
+	while(($line = fgetcsv($handle, 0, ",",'"')) !== false) 
+	{
+		foreach ($line as $key => $val) 
+		{
+			// Don't allow any empty values
+			if($line[$key] == "")
+			{
+				$line[$key] = "0";
+			}
+		}
 
-    $rs = fputcsv($handle_out, $line);
-  }
+		$rs = fputcsv($handle_out, $line);
+	}
 
-  fclose($handle);
-  fclose($handle_out);
+	fclose($handle);
+	fclose($handle_out);
 }
 
 function generate_business_csv($filename)
 {
-  $infile = get_infile($filename);
-  $outfile = get_outfile($filename);
+	$infile = get_infile($filename);
+	$outfile = get_outfile($filename);
 
-  // sanitize: name, city, type, 
-  $handle = fopen($infile, "r");  
-  $handle_out = fopen($outfile, "w"); 
-  
-  $attributes = fgetcsv($handle, 0, ",",'"');
-  $rs = fputcsv($handle_out, $attributes);
-  // echo Debug::vars($attributes); exit;
+	// sanitize: name, city, type, 
+	$handle = fopen($infile, "r");  
+	$handle_out = fopen($outfile, "w"); 
+	
+	$attributes = fgetcsv($handle, 0, ",",'"');
+	$rs = fputcsv($handle_out, $attributes);
+	// echo Debug::vars($attributes); exit;
 
-  while(($line = fgetcsv($handle, 0, ",",'"')) !== false) 
-  {
-    foreach ($line as $key => $val) 
-    {
-      // This value seems to just imply "unknown"
-      if($line[$key] == "{}")
-      {
-        $line[$key] = ""; 
-      }
+	while(($line = fgetcsv($handle, 0, ",",'"')) !== false) 
+	{
+		foreach ($line as $key => $val) 
+		{
+			// This value seems to just imply "unknown"
+			if($line[$key] == "{}")
+			{
+				$line[$key] = ""; 
+			}
 
-      // Assume attributes are UNKNOWN if not specified.
-      // Assume categories & neighborhoods can ONLY be true or false; 
-      if(stripos($attributes[$key], "categories") !== false OR stripos($attributes[$key], "neighborhoods") !== false)
-      {
-        if($line[$key] == "" OR $line[$key] == "{}")
-        {
-          // There's no half-way.
-          // you're in a category, or you're not.
-          // you're in a neighborhood, or you're not.
-          $line[$key] = "F";
-        }
-      }
+			// Assume attributes are UNKNOWN if not specified.
+			// Assume categories & neighborhoods can ONLY be true or false; 
+			if(stripos($attributes[$key], "categories") !== false OR stripos($attributes[$key], "neighborhoods") !== false)
+			{
+				if($line[$key] == "" OR $line[$key] == "{}")
+				{
+					// There's no half-way.
+					// you're in a category, or you're not.
+					// you're in a neighborhood, or you're not.
+					$line[$key] = "F";
+				}
+			}
 
-      if($line[$key] == "True")
-      {
-        $line[$key] = "T";
-      }
+			if($line[$key] == "True")
+			{
+				$line[$key] = "T";
+			}
 
-      if($line[$key] == "False")
-      {
-        $line[$key] = "F";
-      }
-    }
+			if($line[$key] == "False")
+			{
+				$line[$key] = "F";
+			}
+		}
 
-    $rs = fputcsv($handle_out, $line);
-  }
+		$rs = fputcsv($handle_out, $line);
+	}
 
-  fclose($handle);
-  fclose($handle_out);
+	fclose($handle);
+	fclose($handle_out);
 }
 
 function generate_tip_csv($filename)
 {
-  $infile = get_infile($filename);
-  $outfile = get_outfile($filename);
+	$infile = get_infile($filename);
+	$outfile = get_outfile($filename);
 
-  // sanitize: name, city, type, 
-  $handle = fopen($infile, "r");  
-  $handle_out = fopen($outfile, "w"); 
-  
-  $attributes = fgetcsv($handle, 0, ",",'"');
-  $rs = fputcsv($handle_out, $attributes);
-  echo Debug::vars($attributes); exit;
+	// sanitize: name, city, type, 
+	$handle = fopen($infile, "r");  
+	$handle_out = fopen($outfile, "w"); 
+	
+	$attributes = fgetcsv($handle, 0, ",",'"');
+	$rs = fputcsv($handle_out, $attributes);
+	echo Debug::vars($attributes); exit;
 
-  while(($line = fgetcsv($handle, 0, ",",'"')) !== false) 
-  {
-    foreach ($line as $key => $val) 
-    {
-      // Assume attributes are UNKNOWN if not specified.
-      // Assume categories & neighborhoods can ONLY be true or false; 
-      if(stripos($attributes[$key], "categories") !== false OR stripos($attributes[$key], "neighborhoods") !== false)
-      {
-        if($line[$key] == "" OR $line[$key] == "{}")
-        {
-          // There's no half-way.
-          // you're in a category, or you're not.
-          // you're in a neighborhood, or you're not.
-          $line[$key] = "F";
-        }
-      }
+	while(($line = fgetcsv($handle, 0, ",",'"')) !== false) 
+	{
+		foreach ($line as $key => $val) 
+		{
+			// Assume attributes are UNKNOWN if not specified.
+			// Assume categories & neighborhoods can ONLY be true or false; 
+			if(stripos($attributes[$key], "categories") !== false OR stripos($attributes[$key], "neighborhoods") !== false)
+			{
+				if($line[$key] == "" OR $line[$key] == "{}")
+				{
+					// There's no half-way.
+					// you're in a category, or you're not.
+					// you're in a neighborhood, or you're not.
+					$line[$key] = "F";
+				}
+			}
 
-      if($line[$key] == "True")
-      {
-        $line[$key] = "T";
-      }
+			if($line[$key] == "True")
+			{
+				$line[$key] = "T";
+			}
 
-      if($line[$key] == "False")
-      {
-        $line[$key] = "F";
-      }
-    }
+			if($line[$key] == "False")
+			{
+				$line[$key] = "F";
+			}
+		}
 
-    $rs = fputcsv($handle_out, $line);
-  }
+		$rs = fputcsv($handle_out, $line);
+	}
 
-  fclose($handle);
-  fclose($handle_out);
+	fclose($handle);
+	fclose($handle_out);
 }
 
 function generate_review_csv($filename)
 {
-  // Nothing to do
+	// Nothing to do
 }
 
 function generate_checkin_csv($filename)
 {
-  // Nothing to do  
+	// Nothing to do  
 }
 
 
