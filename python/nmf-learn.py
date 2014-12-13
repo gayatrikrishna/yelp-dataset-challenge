704d3305472d9327cdd521d38b0607d83ca1c19f
Jason Feriante <jason.feriante@gmail.com>
2014-12-13 14:51:16 -0600

    Rewrote K-means.... getting higher accuracy now on tests

--- python/nmf-learn.py
+++ python/nmf-learn.py
@@ -11,12 +11,10 @@
 import sys, time, helpers, copy, math
 import pylibmc # use memcached to hold our dataset in memory because it loads way too slow otherwise.
 import numpy as np
+from sklearn.cluster import KMeans
 # http://matplotlib.org/users/pyplot_tutorial.html
 from datetime import datetime
 from scipy.sparse import coo_matrix
-from random import shuffle
-from random import randint
-# np.random.seed(0) #@todo remove this later.......
 
 load_data = helpers.load_data
 get_attributes = helpers.get_attributes
@@ -46,6 +44,7 @@ def get_fulldata_path(num):
         '../data/arff/full_data/yelp_academic_dataset_checkin.arff',
         '../data/arff/full_data/yelp_academic_dataset_review.arff',
         '../data/arff/full_data/yelp_academic_dataset_tip.arff',
+        '../data/arff/_review_matrix/user_review_matrix_jason_big.arff',
         '../data/arff/_review_matrix/user_review_matrix_final2.arff',
         # '../data/arff/_review_matrix/user_review_matrix_jason_massive.arff',
         ]
@@ -75,6 +74,9 @@ def get_baseline(data):
     return baseline, counts
 
 def user_reviews_full():
+    return user_reviews_inner(get_fulldata_path(6))
+
+def user_reviews_full_old():
     return user_reviews_inner(get_fulldata_path(5))
 
 def user_reviews_subset():
@@ -92,6 +94,7 @@ def user_reviews_inner(path):
     biz_index = attributes.get('B0').get('index')
     # the system can only handle numeric values; convert all strings to numbers
     to_predict = []
+    new_dataset = []
     for row in dataset:
         #0 yelping_since string
         date = datetime.strptime(row[0], '%Y-%m-%d')
@@ -119,15 +122,19 @@ def user_reviews_inner(path):
         #20 votes.useful numeric
 
         # wipe out early rows
-        i = 0
-        while(i < biz_index):
-            row[i] = 0
+        i = biz_index
+        _max = len(row)
+        new_row = []
+        while(i < _max):
+            new_row.append(row[i])
             i += 1
+
+        new_dataset.append(new_row)
 
     # extract a random sample from the dataset since 250k users is too much
     # dataset = get_subset(dataset)
 
-    return attributes, dataset
+    return attributes, new_dataset
 
 def update(V, W, H, WH, V_div_WH):
     """ 
@@ -247,8 +254,8 @@ def get_baselines(dataset, normalize):
             # needs to consider business average which I don't have.
             user_avgs[i] = (float(V[i].sum()) / np.count_nonzero(V[i])) - Vexpec
         else:
-            rand = randint(1,9) / 1000.0
-            user_avgs[i] = rand # we know nothing about this user; they had zero test-set ratings
+            # rand = randint(1,9) / 1000.0
+            user_avgs[i] = 0 # we know nothing about this user; they had zero test-set ratings
             cold_start_count += 1
             cold_start_rows.append(i)
 
@@ -319,6 +326,13 @@ def display_results(testset, predict, is_baseline = False):
             if(testset[i][j] > 0):
                 expected = testset[i][j]
                 predicted = predict[i][j]
+                # sanity check on our predictions.....
+                if(predicted > 5):
+                    predicted = 5
+                elif(predicted < 1):
+                    predicted = 1
+                else:
+                    pass
 
                 totalSum += pow(expected - predicted, 2)
                 totalMAE += abs(expected - predicted)
@@ -346,8 +360,8 @@ def generic_baseline(dataset, normalize):
             # needs to consider business average which I don't have.
             user_avgs[i] = (float(V[i].sum()) / np.count_nonzero(V[i])) - Vexpec
         else:
-            rand = randint(1,9) / 1000.0
-            user_avgs[i] = rand # we know nothing about this user; they had zero test-set ratings
+            # rand = randint(1,9) / 1000.0
+            user_avgs[i] = 0 # we know nothing about this user; they had zero test-set ratings
             cold_start_count += 1
             cold_start_rows.append(i)
 
@@ -365,7 +379,7 @@ def generic_baseline(dataset, normalize):
     # normalize our data
     arr_min = np.amin(V)
 
-    const = abs(arr_min) + 0.000001 # avoid zero values
+    const = abs(arr_min) + 0.0001 # avoid zero values
     for i in range(len(dataset)):
         for j in range(len(dataset[0])):
             V[i][j] += const
@@ -375,26 +389,138 @@ def generic_baseline(dataset, normalize):
 
     return V, cold_start_percent, cold_start_rows, const
 
+def count_reviews(dataset):
+    count = 0
+    for i in range(len(dataset)):
+        for j in range(len(dataset[0])):
+            if(dataset[i][j] > 0):
+                count += 1
+            else:
+                pass
+
+    return count
+
+def kmeans_prediction(dataset, n, n_clusters, n_init, attributes, testset, const, normalize):
+    train = coo_matrix(dataset).tocsr()
+
+    # print 'K-Means'
+    k_means = KMeans(n_clusters=n_clusters, init='random', max_iter=n, n_init=n_init, n_jobs=4)
+    k_means.fit_transform(train)
+    kmeans_labels = k_means.labels_
+    kmeans_centers = k_means.cluster_centers_
+    kmeans_inertia = k_means.inertia_
+
+    biz_index = 0
+    # initialize a row to put our predictions in
+    init_row = []
+    for i in range(0, len(dataset[0])):
+        init_row.append(0)
+
+    # initialize cluster predictions
+    init_row = [0.0] * len(dataset[0])
+
+    # init empty data clusters
+    data_clusters = []
+    for i in range(0, n_clusters):
+        data_cluster = []
+        data_clusters.append(data_cluster)
+
+    for i in range(0, len(dataset)):
+        # assign each row to the right cluster
+        data_clusters[kmeans_labels[i]].append(dataset[i])
+
+    print 'Make K-means Predictions'
+    cluster_index = 0
+    cluster_predictions = []
+    for i in range(n_clusters):
+        cluster_predictions.append(init_row)
+
+    # kmeans_labels[0] <--e.g. tells us what cluster each user belongs to
+    # now we can build our prediction matrix for each cluster
+    empty_clusters = 0
+    for d_set in data_clusters:
+        if(len(d_set) > 0):
+            # for each row in this dataset, make a prediction (based on the average rating)
+            # if there's no rating, use the business average rounded to the nearest star
+            # (since we have nothing else to go on)
+            # we will do this column, by column.
+            col = biz_index # optimization
+            while(col < len(d_set[0])):
+                review_count = 0.0
+                total_stars = 0.0
+                avg_stars = 0.0
+                for row in range(len(d_set)):
+                    review_count += 1 
+                    total_stars += d_set[row][col]
+                    
+                if(review_count > 0):
+                    avg_stars = total_stars / review_count
+                else:
+                    pass
+                cluster_predictions[cluster_index][col] = avg_stars
+                col += 1
+        else:
+            empty_clusters += 1
+
+        cluster_index += 1
+
+    print 'number of empty clusters: %d' % (empty_clusters)
+    
+    # now, make predictions for every value in the testset:
+    # (we will filter out zero values later)
+    predict = []
+    for i in range(len(testset)):
+        row = []
+        # which cluster is this user in?
+        cluster_index = kmeans_labels[i]
+        for col in range(len(testset[0])):
+            pred_stars = cluster_predictions[cluster_index][col]
+            row.append(pred_stars)
+        predict.append(row)
+
+    return predict
+
+def run_kmeans(V, trainingset, attributes, testset, const, normalize):
+    n_clusters = int(math.sqrt(len(trainingset) / 2))
+    n_clusters = int(round(n_clusters * 3.5))
+    print 'number of clusters: %d' % (n_clusters)
+    n_init = 10
+    kmeans_iter = 100
+    X = kmeans_prediction(V, kmeans_iter, n_clusters, n_init, attributes, testset, const, normalize)
+    X = np.asarray(X)
+    return denormalize(X, normalize, const)
+
 def main(args):
-    attributes, dataset = user_reviews_full()
+    attributes, dataset = user_reviews_full_old()
+    # attributes, dataset = user_reviews_full()
     # attributes, dataset = user_reviews_subset()
 
     t_height = 0.25
-    t_width = 0.4
+    t_width = 0.5
     normalize = 1.0/100.0
     # normalize = 1.0
-    latent_factors = 20
+    height = len(dataset)
+    width = len(dataset[0])
+    min_ = min(height,width)
+    latent_factors = round(min_ / 3.3)
+
+    # print 'number of latent_factors: %d' % (latent_factors)
+    # exit(0)
+    # iterations = 75
     iterations = 100
 
     trainingset, testset, predict_count = split_maxtrix(dataset, t_height, t_width)
     testset = np.asarray(testset)
 
+    test_review_count = count_reviews(trainingset)
+    train_review_count = count_reviews(testset)
+
     # turn array into numpy array so we can apply their statistical methods
     # V, cold_start_percent, cold_start_rows, const = get_baselines(trainingset, normalize)
     V, cold_start_percent, cold_start_rows, const = get_baselines(trainingset, normalize)
 
-    # generic = copy.deepcopy(trainingset)
-    # Vb, bcold_start_percent, bcold_start_rows, bconst = get_baselines(generic, normalize)
+    generic = copy.deepcopy(trainingset)
+    Vb, bcold_start_percent, bcold_start_rows, bconst = get_baselines(generic, normalize)
 
     # NMF definition & performance:
     # http://arxiv.org/pdf/1205.3193.pdf <--NMF performed best on sparse data.
@@ -416,11 +542,15 @@ def main(args):
     # V = coo_matrix(V).tocsr()
     # V = np.arange(0.01,1.01,0.01).reshape(10,10)
 
+    # print 'Kmeans'
+    # kpredict = run_kmeans(V, trainingset, attributes, testset, const, normalize)
+    # display_results(testset, kpredict)
+
     print 'latent factors: %d' % (latent_factors)
     predict = nmf(V, latent_factors, iterations, const, normalize)
 
     # display baseline results
-    # display_results(testset, denormalize(Vb, normalize, bconst), is_baseline=True)
+    display_results(testset, denormalize(Vb, normalize, bconst), is_baseline=True)
 
     # display overall results
     display_results(testset, predict)
@@ -430,7 +560,6 @@ def main(args):
     # where a value exists in the testset
 
     # compare to baseline.......
-
 
 
 if __name__ == "__main__":
